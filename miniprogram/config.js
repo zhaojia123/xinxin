9eab894e9c2032128b27309165e8cb92c6e22ffe
赵佳 <905518706@qq.com>
2026-09-19 08:23:21 +0800

    修改

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,8 +1,8 @@
-// 开发版连接本机服务；体验版和正式版连接线上 HTTPS 服务。
+// 上线后各版本统一连接线上 HTTPS 服务；本地联调时再临时改回本机地址。
 module.exports = {
   name: '欣欣的日常记录',
   filingNumber: '陇ICP备2026008700号-2',
-  develop: 'http://127.0.0.1:8999',
+  develop: 'https://xxtaizhang.cn',
   trial: 'https://xxtaizhang.cn',
   release: 'https://xxtaizhang.cn'
 }
